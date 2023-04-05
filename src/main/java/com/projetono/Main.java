ec7f811fe0a969e5493df1c2fafdfb94af90c78a
oMarcoMaciel <marcomaciel06@gmail.com>
2023-04-05 10:12:41 -0300

    Initial commit

---
+++ src/main/java/com/projetono/Main.java
@@ -0,0 +1,32 @@
+package com.projetono;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        No<String> no1 = new No<>("conteudo no1");
+
+        No<String> no2 = new No<>("conteudo no2");
+        no1.setProximoNo(no2);
+
+        No<String> no3 = new No<>("conteudo no3");
+        no2.setProximoNo(no3);
+
+        No<String> no4 = new No<>("conteudo no3");
+        no3.setProximoNo(no4);
+
+        System.out.println(no1);
+        System.out.println(no1.getproximoNo());
+        System.out.println(no2);
+
+        System.out.println("-----------");
+
+        System.out.println(no1);
+        System.out.println(no1.getproximoNo().getproximoNo().getproximoNo());
+
+    }
+}
+
+// no1 apontando p no2 assim em diante até no4
+// apontar para nulo
+
